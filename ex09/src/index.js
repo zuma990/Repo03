63db26952dcf462f4da2a2cad56377fd2cd75902
zuma990 <zumajuve@gmail.com>
2022-03-30 20:23:16 +0200

    Added new file

---
+++ ex09/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo) {
+  // Only change code below this line
+  var numOne;
+  var numTwo;
+  difference = numOne - numTwo;
+
+  // Only change code above this line
+  return difference;
+}
+console.log(main(15, 8));
+module.exports = main;
