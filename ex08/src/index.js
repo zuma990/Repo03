2f165b7182a482c4afa82211441fc44844dfc498
zuma990 <zumajuve@gmail.com>
2022-03-30 19:50:20 +0200

    Added new file

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo) {
+  // Only change code below this line
+  numOne = 6;
+  numTwo = 15;
+  sum = numOne + numTwo;
+
+  // Only change code above this line
+  return sum;
+}
+console.log(main());
+module.exports = main;
