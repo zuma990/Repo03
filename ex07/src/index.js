a5912789a7e7c1b1def14ded1140e0ee076de6d1
zuma990 <zumajuve@gmail.com>
2022-03-30 19:39:33 +0200

    Added new file

---
+++ ex07/src/index.js
@@ -0,0 +1,21 @@
+function main() {
+  // Only change code below this line
+  // Variable declarations
+  var studlyCapVar;
+  var properCamelCase;
+  var titleCaseOver;
+
+  // Variable assignments
+  studlyCapVar = 10;
+  properCamelCase = "A String";
+  titleCaseOver = 9000;
+
+  // Only change code above this line
+  return {
+    studlyCapVar,
+    properCamelCase,
+    titleCaseOver,
+  };
+}
+console.log(main());
+module.exports = main;
