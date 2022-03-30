88cfccfbe7d8ccba1de52501c1305b34d42b1320
zuma990 <zumajuve@gmail.com>
2022-03-30 18:29:57 +0200

    Added new file

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  // Only change code below this line
+  // some comments
+  /* some comments in block comments
+   */
+  // Only change code above this line
+}
+console.log(main());
+module.exports = main;
